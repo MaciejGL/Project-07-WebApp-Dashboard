21962b15cdf3ffa8a4613b360ef039641e93d56a
MaciejGL <m.glowacki01@gmail.com>
2020-04-08 19:52:47 +0200

    Upload new charts and fix some issues with css

--- plugins/charts.js
+++ plugins/charts.js
@@ -1,14 +1,49 @@
+// =====================================================================================================
+// Elements ============================================================================================
+// =====================================================================================================
+
 const trafficCanvas = document.getElementById("traffic-chart");
 const dailyCanvas = document.getElementById('daily-chart');
 const mobileCanvas = document.getElementById('mobile-chart')
+const liCharts = document.getElementById('chartDataDisplay');
+const liAll = document.querySelectorAll('#chartDataDisplay li');
 
-
+// DATA FOR TRAFFIC CHART ==============================================================================
+// DATA FOR TRAFFIC CHART ==============================================================================
+// DATA FOR TRAFFIC CHART ==============================================================================
 
 Chart.defaults.global.defaultFontSize = 12
 
-
-// DATA FOR TRAFFIC CHART
-
+let trafficDataHourly = {
+    labels: ['0-6', '6-12', '12-18', '18-24'],
+    datasets: [{
+        data: [56, 46, 363, 44],
+        backgroundColor: 'rgba(116, 119, 191, .2)',
+        borderWidth: 1,
+        borderColor: 'rgba(116, 119, 191, 1)',
+        fill: 'start',
+        lineTension: 0,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointBorderColor: 'rgba(116, 119, 191, 1)',
+        pointRadius: 4,
+    }, ]
+}
+let trafficDataDaily = {
+    labels: ['1', '2', '3', '4', '5', '6', '7'],
+    datasets: [{
+        data: [22, 50, 100, 100, 150, 10, 12],
+        backgroundColor: 'rgba(116, 119, 191, .2)',
+        borderWidth: 1,
+        borderColor: 'rgba(116, 119, 191, 1)',
+        fill: 'start',
+        lineTension: 0,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointBorderColor: 'rgba(116, 119, 191, 1)',
+        pointRadius: 4,
+    }]
+}
 
 let trafficDataWeekly = {
     labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
@@ -26,7 +61,21 @@ let trafficDataWeekly = {
     }]
 }
 
-
+let trafficDataMonthly = {
+    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+    datasets: [{
+        data: [33, 12350, 10010, 20200, 15040, 13750, 12520, 18250, 22050, 15000, 25000, 200],
+        backgroundColor: 'rgba(116, 119, 191, .2)',
+        borderWidth: 1,
+        borderColor: 'rgba(116, 119, 191, 1)',
+        fill: 'start',
+        lineTension: 0,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointBorderColor: 'rgba(116, 119, 191, 1)',
+        pointRadius: 4,
+    }]
+}
 
 let trafficOptions = {
     aspectRatio: 3,
@@ -41,7 +90,7 @@ let trafficOptions = {
         yAxes: [{
             ticks: {
                 beginAtZero: true,
-                stepSize: 500,
+                // stepSize: 500,
                 fontSize: 10,
                 padding: 10,
             },
@@ -63,14 +112,40 @@ let trafficOptions = {
     }
 };
 
-
-//  TRAFFIC CHART
+// =====================================================================================================
+// TRAFFIC CHART =======================================================================================
+// =====================================================================================================
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
-    data: trafficDataWeekly,
+    data: trafficDataHourly,
     options: trafficOptions,
 });
 
+
+const renderChart = (data, e) => {
+    trafficChart.destroy();
+    trafficChart = new Chart(trafficCanvas, {
+        type: 'line',
+        data: data,
+        options: trafficOptions,
+    });
+    e.target.classList.add('active')
+}
+
+liCharts.addEventListener('click', (e) => {
+    liAll.forEach(li => {
+        li.classList.remove('active')
+    })
+    if (e.target.textContent == 'Hourly') {
+        renderChart(trafficDataHourly, e)
+    } else if (e.target.textContent == 'Daily') {
+        renderChart(trafficDataDaily, e)
+    } else if (e.target.textContent == 'Weekly') {
+        renderChart(trafficDataWeekly, e)
+    } else if (e.target.textContent == 'Monthly') {
+        renderChart(trafficDataMonthly, e)
+    }
+})
 //  DAILY CANVAS
 
 const dailyData = {
