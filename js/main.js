e9b038f68f54a2e56b292bc061883b5614139e10
MaciejGL <58422989+MaciejGL@users.noreply.github.com>
2020-05-11 18:51:05 +0200

    Validate HTML CSS AND JS

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ alertBanner.innerHTML =
 alertBanner.addEventListener('click', (e) => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
-        alertBanner.style.display = "none"
+        alertBanner.style.display = "none";
     }
-})
+});
 
 
 
@@ -27,17 +27,17 @@ const send = document.getElementById('send');
 
 
 send.addEventListener('click', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!user.value && !message.value) {
-        alert('Please fill out user and message fields before sending')
+        alert('Please fill out user and message fields before sending');
     } else if (!user.value) {
-        alert('Please fill out user field before sending')
+        alert('Please fill out user field before sending');
     } else if (!message.value) {
-        alert('Please fill out message field before sending')
+        alert('Please fill out message field before sending');
     } else {
-        alert(`Message successfully send to: ${user.value}`)
+        alert(`Message successfully send to: ${user.value}`);
         message.value = "";
-        user.value = ""
+        user.value = "";
     }
 });
 
@@ -54,19 +54,19 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function () {
     modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
 
 
 
@@ -75,88 +75,78 @@ window.onclick = function (event) {
 
 
 
-const sectionSettings = document.querySelector('#settings')
+const sectionSettings = document.querySelector('#settings');
 const onOffEmail = document.querySelector('#email');
 const onOffPublic = document.querySelector('#public');
 const selectTimezone = document.querySelector('#timezone');
 const save = document.querySelector('#save');
 const cancel = document.querySelector('#cancel');
 
-let emailNotification = localStorage.getItem('emailNotification')
-let publicProfile = localStorage.getItem('publicProfile')
-let timeZone = localStorage.getItem('timeZone')
-
-
-
-
-const cancelSetting = () => {
-    // emailNotification.addEventListener('change', ())
-}
-
-
+let emailNotification = localStorage.getItem('emailNotification');
+let publicProfile = localStorage.getItem('publicProfile');
+let timeZone = localStorage.getItem('timeZone');
 
 
 
 const getSwitcherData = (localData, switcher) => {
     if (localData == 'true') {
-        switcher.checked = true
+        switcher.checked = true;
     } else {
-        switcher.checked = false
+        switcher.checked = false;
     }
-}
+};
 
 const getSelectData = (localData, select) => {
     if (localData) {
-        select.value = timeZone
+        select.value = timeZone;
     }
-}
+};
 
 window.addEventListener('load', () => {
     getSwitcherData(emailNotification, onOffEmail);
     getSwitcherData(publicProfile, onOffPublic);
     getSelectData(timeZone, selectTimezone);
-})
+});
 
 
 const setSettings = () => {
     if (onOffEmail.checked) {
-        emailNotification = true
+        emailNotification = true;
     }
     if (!onOffEmail.checked) {
-        emailNotification = false
+        emailNotification = false;
     }
     if (onOffPublic.checked) {
-        publicProfile = true
+        publicProfile = true;
     }
     if (!onOffPublic.checked) {
         publicProfile = false;
-    };
+    }
     if (selectTimezone) {
-        timeZone = selectTimezone.value
+        timeZone = selectTimezone.value;
     }
 
     if (publicProfile.toString() !== localStorage.getItem('publicProfile') || emailNotification.toString() !== localStorage.getItem('emailNotification') || timeZone.toString() !== localStorage.getItem('timeZone')) {
-        cancel.disabled = false
+        cancel.disabled = false;
     } else {
-        cancel.disabled = true
+        cancel.disabled = true;
     }
+};
 
-}
 
-
-sectionSettings.addEventListener('click', setSettings)
+sectionSettings.addEventListener('click', setSettings);
 
 save.addEventListener('click', () => {
-    localStorage.setItem('emailNotification', emailNotification)
-    localStorage.setItem('publicProfile', publicProfile)
-    localStorage.setItem('timeZone', timeZone)
-})
+    localStorage.setItem('emailNotification', emailNotification);
+    localStorage.setItem('publicProfile', publicProfile);
+    localStorage.setItem('timeZone', timeZone);
+});
 
 cancel.addEventListener('click', () => {
-    emailNotification = localStorage.getItem('emailNotification')
-    publicProfile = localStorage.getItem('publicProfile')
-    timeZone = localStorage.getItem('timeZone')
+    emailNotification = localStorage.getItem('emailNotification');
+    publicProfile = localStorage.getItem('publicProfile');
+    timeZone = localStorage.getItem('timeZone');
     getSwitcherData(emailNotification, onOffEmail);
     getSwitcherData(publicProfile, onOffPublic);
     getSelectData(timeZone, selectTimezone);
-})
+});
